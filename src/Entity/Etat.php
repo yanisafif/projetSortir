07d7a8491736642d05db1e74bb01661f0aa74254
laurloustic <laurence.langouet2021@campus-eni.fr>
2021-07-14 18:24:15 +0200

    created entity etat

---
+++ src/Entity/Etat.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EtatRepository::class)
+ */
+class Etat
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idEtat;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $libelle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdEtat(): ?int
+    {
+        return $this->idEtat;
+    }
+
+    public function setIdEtat(int $idEtat): self
+    {
+        $this->idEtat = $idEtat;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+}
