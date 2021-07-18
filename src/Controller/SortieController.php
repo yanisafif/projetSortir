226139f2618baae1ba24377c1c6a4e86c5dcd4c2
laurloustic <laurence.langouet2021@campus-eni.fr>
2021-07-18 22:58:52 +0200

    created detail sortie controller and template

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Sortie;
 use App\Entity\Campus;
+use App\Entity\Participant;
 
 class SortieController extends AbstractController
 {
@@ -25,6 +26,24 @@ class SortieController extends AbstractController
         return $this->render('sortie/index.html.twig', [
             'sorties' => $sorties,
             'campus'=> $campus,
+        ]);
+    }
+
+    /**
+     * @Route("/sortie/detail/{id}", name="detailsortie")
+     */
+    public function detailSortie($id): Response
+    {
+        $sortie = $this->getDoctrine()
+            ->getRepository(Sortie::class)
+            ->findById($id);
+        $participants = $sortie[0]->getParticipants();
+
+
+        return $this->render('sortie/detail.html.twig', [
+            'controller_name' => 'EnregistrementController',
+            'sortie'=>$sortie,
+            'participants'=>$participants,
         ]);
     }
 }
