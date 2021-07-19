405e5634841f05575b3629760e0d772b83ec1193
SarahGeo33 <sarah.geoffroy2021@campus-eni.fr>
2021-07-19 23:11:19 +0200

    création formulaire création sortie

---
+++ src/Form/SortieType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('dateHeureDebut')
+            ->add('duree')
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionsMax')
+            ->add('infosSortie')
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'name',
+                'placeholder' => 'Campus'
+            ])
+            ->add('etat')
+            ->add('participants')
+            ->add('participant')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
